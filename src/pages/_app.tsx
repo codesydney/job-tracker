42b82734fd24b416f0bde54698455a9612ef3b27
David Taing <adavidtaing@gmail.com>
2022-12-08 18:29:06 +1100

    build: add and setup Mock Service Worker
    Changes:
    - Add and setup msw package
    - Add GET /api/hello-world mock handler

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+
+const API_MOCKING = true;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [shouldRender, setShouldRender] = useState(API_MOCKING);
+
+  useEffect(() => {
+    async function initMocks() {
+      const { initMocks } = await import("../mocks");
+      await initMocks();
+      setShouldRender(true);
+    }
+
+    if (API_MOCKING) {
+      initMocks();
+    }
+  }, []);
+
+  if (!shouldRender) {
+    return null;
+  }
+
+  return <Component {...pageProps} />;
 }
