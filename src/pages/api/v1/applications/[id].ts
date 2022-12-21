6138ad8131e8d01a0eec807d2e2bd2c7e277685f
David Taing <adavidtaing@gmail.com>
2022-12-21 21:29:27 +1100

    refactor: add arguments for helper functions
    Changes:
    - Move common body.id logic to main handler
    - Add explicit arguments for helper functions

--- src/pages/api/v1/applications/[id].ts
+++ src/pages/api/v1/applications/[id].ts
@@ -1,20 +1,18 @@
 import prisma from "../../../../lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
+import { Prisma } from "@prisma/client";
 
-const getApplication = async (req: NextApiRequest, res: NextApiResponse) => {
+const getApplication = async (id: number, res: NextApiResponse) => {
   try {
-    if (typeof req.query.id === "string") {
-      const { id } = req.query;
-      const data = await prisma.application.findUnique({
-        where: { id: parseInt(id) },
-      });
-      if (data) {
-        res.status(200).json(data);
-      } else {
-        res
-          .status(404)
-          .json({ message: `Could not find Application with id ${id}` });
-      }
+    const data = await prisma.application.findUnique({
+      where: { id },
+    });
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res
+        .status(404)
+        .json({ message: `Could not find Application with id ${id}` });
     }
   } catch (error) {
     res.status(400).json({
@@ -23,65 +21,62 @@ const getApplication = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 };
 
-const updateHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+type UpdateApplicationsRequest = Prisma.ApplicationUpdateInput;
+
+const updateHandler = async (
+  id: number,
+  input: UpdateApplicationsRequest,
+  res: NextApiResponse
+) => {
   try {
-    if (typeof req.query.id === "string") {
-      const { id } = req.query;
-      const { status } = req.body;
-      const data = await prisma.application.update({
-        where: { id: parseInt(id) },
-        data: { status },
-      });
-      if (data) {
-        res.status(200).json(data);
-      }
+    const data = await prisma.application.update({
+      where: { id },
+      data: input,
+    });
+    if (data) {
+      res.status(200).json(data);
     }
   } catch (error) {
-    const { id } = req.query;
-    if (Number(id)) {
-      res.status(404).json({
-        message: `Could not find Application with id ${id}`,
-      });
-    } else {
-      res.status(400).json({
-        message: `Invalid Input: 'id' was not a number`,
-      });
-    }
+    res.status(404).json({
+      message: `Could not find Application with id ${id}`,
+    });
   }
 };
 
-const deleteHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { id } = req.query;
+const deleteHandler = async (id: number, res: NextApiResponse) => {
   try {
-    if (typeof id === "string") {
-      const data = await prisma.application.delete({
-        where: { id: parseInt(id) },
-      });
-      if (data) {
-        res.status(200).json(data);
-      }
+    const data = await prisma.application.delete({
+      where: { id },
+    });
+
+    if (data) {
+      res.status(200).json(data);
     }
   } catch (error) {
-    const { id } = req.query;
-    if (Number(id)) {
-      res.status(404).json({
-        message: `Could not find Application with id ${id}`,
-      });
-    } else {
-      res.status(400).json({
-        message: `Invalid Input: 'id' was not a number`,
-      });
-    }
+    res.status(404).json({
+      message: `Could not find Application with id ${id}`,
+    });
   }
 };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id: rawId } = req.query as { id: string };
+
+  const id = parseInt(rawId);
+
+  if (Number.isNaN(id)) {
+    res.status(400).json({
+      message: `Invalid Input: 'id' was not a number`,
+    });
+  }
+
   if (req.method === "GET") {
-    getApplication(req, res);
+    getApplication(id, res);
   } else if (req.method === "PATCH") {
-    updateHandler(req, res);
+    const { status } = req.body;
+    updateHandler(id, { status }, res);
   } else if (req.method === "DELETE") {
-    deleteHandler(req, res);
+    deleteHandler(id, res);
   }
 };
 
