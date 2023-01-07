e494efb20853b974ce6435a931b99551cb3db874
Sagar Dobariya <infodsagar@gmail.com>
2023-01-07 17:04:35 +1100

    devides endpoint into smaller functions

--- src/pages/api/v1/job-descriptions/index.ts
+++ src/pages/api/v1/job-descriptions/index.ts
@@ -1,30 +1,45 @@
 import prisma from '../../../../lib/prisma';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'GET') {
-    try {
-      const data = await prisma.jobDescription.findMany();
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(500).json({
-        message:
-          'Something went wrong on our side and we failed to get Job Descriptions.',
-      });
-    }
-  } else if (req.method === 'POST') {
-    try {
-      const { rawText } = req.body;
-      console.log(rawText);
-      const data = await prisma.jobDescription.create({
-        data: { rawText },
-      });
-
-      res.status(201).json(data);
-    } catch (error) {
-      res.status(500).json({
-        message: `Failed to create Job Description: invalid rawText field`,
-      });
-    }
+const getJobDescriptions = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  try {
+    const data = await prisma.jobDescription.findMany();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      message:
+        'Something went wrong on our side and we failed to get Job Descriptions.',
+    });
+  }
+  if (req.method === 'POST') {
   }
 };
+
+const postHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { rawText } = req.body;
+    console.log(rawText);
+    const data = await prisma.jobDescription.create({
+      data: { rawText },
+    });
+
+    res.status(201).json(data);
+  } catch (error) {
+    res.status(500).json({
+      message: `Failed to create Job Description: invalid rawText field`,
+    });
+  }
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'GET') {
+    getJobDescriptions(req, res);
+  } else if (req.method === 'PATCH') {
+    postHandler(req, res);
+  }
+};
+
+export default handler;
